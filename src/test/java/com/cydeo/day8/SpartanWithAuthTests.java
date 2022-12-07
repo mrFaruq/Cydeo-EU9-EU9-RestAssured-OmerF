aceeed3cb137b313a0d7a83f1df8e2b98ad9c65b
omerf <omeryavuncu@gmail.com>
2022-12-07 14:26:36 +0000

    Day 10

--- src/test/java/com/cydeo/day8/SpartanWithAuthTests.java
+++ src/test/java/com/cydeo/day8/SpartanWithAuthTests.java
@@ -24,7 +24,7 @@ public class SpartanWithAuthTests extends SpartanAuthTestBase {
 
     @DisplayName("GET/api/spartans as admin user expect 200")
     @Test
-    public void test2(){
+    public void testAdmin(){
 
         //how to pass admin, admin as a username and password?
         given()
@@ -37,5 +37,32 @@ public class SpartanWithAuthTests extends SpartanAuthTestBase {
                 .log().all();
     }
 
+    @DisplayName("DELETE/spartans/{id} as editor user expect 403")
+    @Test
+    public void testEditorDelete(){
+
+        given()
+                .auth().basic("editor","editor")
+                .and().accept(ContentType.JSON)
+                .and().pathParam("id",30)
+        .when()
+                .delete("api/spartans/{id}")
+        .then()
+                .statusCode(403)
+                .log().all();
+    }
+
 
 }
+
+/*
+        As a homework,write a detailed test for Role Base Access Control(RBAC)
+            in Spartan Auth app (7000)
+            Admin should be able to take all CRUD (Create, Read, Update, Delete)
+            Editor should be able to take all CRUD
+                other than DELETE
+            User should be able to only READ data
+                not update,delete,create (POST,PUT,PATCH,DELETE)
+       --------------------------------------------------------
+        Can guest even read data ? 401 for all
+     */
