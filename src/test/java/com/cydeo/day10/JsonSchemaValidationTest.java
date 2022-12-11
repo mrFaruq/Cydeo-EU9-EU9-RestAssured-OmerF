dc46cd7766b23cdbb955126e117a54f8d283736b
omerf <omeryavuncu@gmail.com>
2022-12-11 22:42:58 +0000

    Day 11

--- src/test/java/com/cydeo/day10/JsonSchemaValidationTest.java
+++ src/test/java/com/cydeo/day10/JsonSchemaValidationTest.java
@@ -6,6 +6,8 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -28,7 +30,26 @@ public class JsonSchemaValidationTest extends SpartanAuthTestBase {
                 .statusCode(200)
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"))
                 .log().all();
+    }
 
+    @DisplayName("GET request to all spartans and verify schema")
+    @Test
+    public void allSpartanSchemaTest(){
+
+        given()
+                .accept(ContentType.JSON)
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans")
+                .then()
+                .statusCode(200)
+                //what if you have your .json file not under resources following way -->
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/com/cydeo/day10/allSpartansSchema.json")));
 
     }
+
+    //homework
+    //put your post json schema under day10
+    //post one spartan using dynamic input(name,gender,phone)
+    //verify your post response matching with json schema
 }
